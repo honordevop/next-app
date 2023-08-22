7c2fb930c3022f8d3d719f5f645eb1ff785537eb
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-08-22 12:24:32 +0100

    Code: created navigation and styles file

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Contact = () => {
-  return <div>About</div>;
+  return <div>Contact</div>;
 };
 
 export default Contact;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,10 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">
-        <Navbar />
-        {children}
-        <Footer />
+      <body className={inter.className}>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
