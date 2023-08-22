c86438f7e42aef756f743bbc6476e26893c57a67
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-08-22 11:22:02 +0100

    File: sub-route folders created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">OrnorIT</Link>
+    </div>
+  );
+};
+
+export default Navbar;
