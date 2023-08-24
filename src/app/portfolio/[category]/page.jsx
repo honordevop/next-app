132b58c215fd5664d9dfdb364fb8b1e1364509dd
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-08-24 10:54:48 +0100

    Code: Implemented local data fecth of portfolio page data

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,53 +2,43 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
 
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-            veritatis porro beatae expedita consequuntur aliquam voluptatem,
-            iste eos consequatur quos quis assumenda animi quisquam, cum saepe
-            deserunt modi, mollitia obcaecati?
-          </p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              src={item.image}
+              alt={`${params.category} image`}
+              fill={true}
+              className={styles.img}
+              priority
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/17267137/pexels-photo-17267137/free-photo-of-istanbul-cityscape-panorama-with-ships-in-a-bosphorus-strait.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            reiciendis similique placeat ea quae illum debitis, eius velit neque
-            nemo natus laudantium aperiam voluptatem, totam quibusdam aliquam
-            maxime dicta aut.
-          </p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/17267137/pexels-photo-17267137/free-photo-of-istanbul-cityscape-panorama-with-ships-in-a-bosphorus-strait.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
