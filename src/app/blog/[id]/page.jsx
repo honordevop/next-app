7b1396c5942a9742915b6c30109255757c530bbe
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-08-25 08:44:02 +0100

    Code: implemented dynamic metadata generate for post

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { Metadata } from "next";
 
 async function getData(id) {
   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
@@ -13,6 +14,14 @@ async function getData(id) {
   }
 
   return res.json();
+}
+
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: post.title,
+    description: post.desc,
+  };
 }
 
 const BlogPost = async ({ params }) => {
